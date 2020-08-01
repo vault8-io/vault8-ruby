0084cc5935831aca47f57e3ac8fdde59d3ddf82f
Sergey Konev <sergey@cimon.io>
2020-08-01 16:58:56 +0300

    Update deps and add rubocop

--- lib/vault8.rb
+++ lib/vault8.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'vault8/version'
 require 'json'
 require 'uri'
@@ -6,11 +8,12 @@ require 'net/http/post/multipart'
 require 'digest'
 require 'tempfile'
 
+# Vault8 is a class providing interface to handle file uploads.
 class Vault8
   attr_reader :public_key, :secret_key, :service_url
 
-  def self.create!(public_key: , secret_key: , service_url:)
-    self.new(public_key, secret_key, service_url)
+  def self.create!(public_key:, secret_key:, service_url:)
+    new(public_key, secret_key, service_url)
   end
 
   def initialize(public_key, secret_key, service_url)
@@ -20,12 +23,12 @@ class Vault8
   end
 
   def image_url(uid:, filters: [], image_name: 'image.jpg', current_time: nil, until_time: nil)
-    generate_url_for( path: image_path(uid, filters, image_name),
-                      current_time: (current_time && current_time.to_i),
-                      until_time: (until_time && until_time.to_i) )
+    generate_url_for(path: image_path(uid, filters, image_name),
+                     current_time: (current_time && current_time.to_i),
+                     until_time: (until_time && until_time.to_i))
   end
 
-  def upload_url(path: '/upload', current_time: Time.now, until_time: Time.now + 86400)
+  def upload_url(path: '/upload', current_time: Time.now, until_time: Time.now + 86_400)
     generate_url_for(path: path, current_time: current_time.to_i, until_time: until_time.to_i)
   end
 
@@ -33,12 +36,13 @@ class Vault8
     Digest::SHA1.hexdigest([public_key, secret_key, path, current_time, until_time].compact.join('|')).reverse
   end
 
-  def image_path(uid, filters=[], image_name='image.jpg')
+  def image_path(uid, filters = [], image_name = 'image.jpg')
     '/' + [uid, merged_filters(filters), escape(escape(image_name), '[]')].compact.join('/')
   end
 
-  def merged_filters(filters=[])
+  def merged_filters(filters = [])
     return nil if filters.empty?
+
     filters.flat_map do |filter|
       filter.map do |k, v|
         [k, v.to_s.empty? ? nil : v].compact.join('-')
@@ -49,22 +53,24 @@ class Vault8
   def generate_url_for(path:, current_time: nil, until_time: nil)
     path = escape(path)
     uri = URI.join(service_url, path)
-    uri.query = { p: public_key,
-                  s: encode_token(path: path, current_time: current_time, until_time: until_time),
-                  time: current_time,
-                  until: until_time
-                }.reduce([]) {|acc, x| acc << "#{x.first}=#{x.last}" unless x.last.nil?; acc}.join('&')
+    uri.query = {
+      p: public_key,
+      s: encode_token(path: path, current_time: current_time, until_time: until_time),
+      time: current_time,
+      until: until_time
+    }.each_with_object([]) { |(k, v), acc| acc << "#{k}=#{v}" unless v.nil? }.join('&')
+
     uri.to_s
   end
 
   def upload_image(file)
-    options = if file.kind_of? String
-      { url: file }
-    elsif file.respond_to?(:tempfile)
-      { file: file }
-    elsif file.kind_of?(File) || file.kind_of?(Tempfile)
-      { file: File.new(file) }
-    end
+    options = if file.is_a?(String)
+                { url: file }
+              elsif file.respond_to?(:tempfile)
+                { file: file }
+              elsif file.is_a?(File) || file.is_a?(Tempfile)
+                { file: File.new(file) }
+              end
 
     JSON.parse(post_request(options))
   rescue JSON::ParserError
@@ -75,6 +81,7 @@ class Vault8
 
   def post_request(options = {})
     return post_link(options) if options[:url]
+
     post_file(options)
   end
 
@@ -93,7 +100,7 @@ class Vault8
 
   # TODO: add real mime-type check
   def mime_for_file(file)
-    "image/#{File.extname(file)[1..-1]}"
+    "image/#{File.extname(file)[1..]}"
   end
 
   def upload_uri
